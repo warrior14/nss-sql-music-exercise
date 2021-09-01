21dc3d8b2c6622c079ea7fedb87432b0c9271087
warrior14 <ironaddicts14@gmail.com>
2021-08-31 22:58:45 -0500

    working on exercises

--- QueryExercise.sql
+++ QueryExercise.sql
@@ -1,2 +1,28 @@
 -- Exercises 
 
+-- Query all of the entries in the Genre table
+SELECT * FROM Genre;
+
+-- Query all the entries in the Artist table and order by the artist's name. HINT: use the ORDER BY keywords
+--Default is ascending (ASC keyword), DESC is descending order
+SELECT 
+	Id,
+	ArtistName,
+	YearEstablished
+FROM Artist
+ORDER BY ArtistName ASC;
+
+-- Write a SELECT query that lists all the songs in the Song table and include the Artist name
+SELECT 
+	Title,
+	ArtistName
+FROM Song
+INNER JOIN Artist
+ON Song.ArtistId = Artist.Id;
+
+-- Write a SELECT query that lists all the Artists that have a Soul Album
+--Different ways to rename a column/property on a table
+
+SELECT 
+    ArtistName "Artist Name",
+    Name AS
